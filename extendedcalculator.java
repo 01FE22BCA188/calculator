442681c49a88683d440ed908436d0b3c5235ddc3
sumeet <01fe22bca188>
2024-02-14 15:14:34 +0530

    extended calc

---
+++ extendedcalculator.java
@@ -0,0 +1,17 @@
+public class extendedcalculator {
+    float square(float operand1){
+        
+            return(operand1*operand1);
+
+    }
+    float cube(float operand1){
+        return(operand1*operand1*operand1);
+    }
+    float modulus(float operand1,float operand2){
+        return(operand1%operand2);
+    }
+    double area_of_tri(double base,double height){
+        return(0.5*base*height);
+    }
+    
+}
